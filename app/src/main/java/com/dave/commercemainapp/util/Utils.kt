f881a3d2e1a00eb4267cbf9b8c12faff7ae76fa9
jongmun <jongmun.choi@illuminarean.com>
2025-05-15 09:39:22 +0900

    [ETC] Enum  Type 외에 타입이 내려왔을시 방어 코드

--- app/src/main/java/com/dave/commercemainapp/util/Utils.kt
+++ app/src/main/java/com/dave/commercemainapp/util/Utils.kt
@@ -6,6 +6,14 @@ object Utils {
         return String.format("%,d원", this)
     }
 
+    inline fun <reified T : Enum<T>> safeValueOf(type: String, default: T): T {
+        return try {
+            java.lang.Enum.valueOf(T::class.java, type)
+        } catch (e: IllegalArgumentException) {
+            default
+        }
+    }
+
     enum class SectionType(typeString: String) {
         HORIZONTAL("horizontal"),
         VERTICAL("vertical"),
